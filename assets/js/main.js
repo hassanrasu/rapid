dd19218047454c97788337985725a0623f4ede3e
Hassan Rasu <hassanrasu04@gmail.com>
2026-03-04 14:14:23 +0600

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,64 @@
 (function ($) {
     "use strict";
     jQuery(document).ready(function ($) {
+        
+          //Mobile Menu Start
+        $(document).ready(function () {
+            $(".has-submenu ").prepend("<span></span>");
+        });
+        $(document).on('click', '.has-submenu > span', function (event) {
+            $(this).parent().find("ul").toggleClass("active");
+            $(this).parent().toggleClass("active");
+        });
+
+        $(".mainmenu ul li:has(ul)").addClass("has-submenu");
+        $(".mainmenu ul li:has(ul)").addClass("small-submenu");
+        $(".mainmenu ul li ul").addClass("sub-menu");
+        $(".mainmenu ul.dropdown li").hover(function () {
+            $(this).addClass("hover");
+        }, function () {
+            $(this).removeClass("hover");
+        });
+        var $menu = $("#menu"),
+            $menulink = $("#menu-toggle"),
+            $header = $(".header-area"),
+            $searchTrigger = $(".searchToggle"),
+            $menuTriggercont = $("#menu_handler"),
+            $menuTrigger = $(".has-submenu > span"),
+            $body = $("body"),
+            $megamenuTrigger = $(".megamenu > li > span");
+        $menulink.click(function (e) {
+            $menulink.toggleClass("active");
+            $menu.toggleClass("active");
+            $menuTriggercont.toggleClass("active");
+            $header.toggleClass("active");
+            $body.toggleClass("active");
+        });
+
+        $menuTrigger.click(function (e) {
+            e.preventDefault();
+            var t = $(this).next();
+            t.toggleClass("active");
+            t.toggleClass("active").next("ul").toggleClass("active");
+            t.toggleClass("active").next(".megamenu-holder").toggleClass("active");
+        });
+
+        $megamenuTrigger.click(function (e) {
+            e.preventDefault();
+            var t = $(this).next();
+            t.toggleClass("active").next(".mega-submenu").toggleClass("active");
+        });
+
+        $searchTrigger.click(function (e) {
+            $menulink.removeClass("active");
+            $menu.removeClass("active");
+
+            $menuTriggercont.removeClass("active");
+        });
+
+        $(".mainmenu ul li:has(ul)");
+        //Mobile Menu End
+
 
     });
 }(jQuery));
